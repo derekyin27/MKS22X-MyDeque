263262c9001991421a2b87fc4a3dff9b20f41708
Derek <dyin00@stuy.edu>
2019-03-21 11:48:42 -0400

    remove and get first last

--- MyDeque.java
+++ MyDeque.java
@@ -4,9 +4,16 @@ private int size, start, end;
 @SuppressWarnings("unchecked")
 public MyDeque(){
   data = (E[])new Object[10];
+  size=10;
+  start = 0;
+  end = data.length;
   }
+@SuppressWarnings("unchecked")
 public MyDeque(int initialCapacity){
+  data = (E[])new Object[size];
   size = initialCapacity;
+  start = 0;
+  end = data.length;
   }
 public int size(){
   return size;
@@ -18,10 +25,26 @@ public String toString(){
   }
   return str;
  }
-public void addFirst(E element){ }
+public void addFirst(E element){
+
+ }
 public void addLast(E element){ }
-public E removeFirst(){ }
-public E removeLast(){ }
-public E getFirst(){ }
-public E getLast(){ }
+public E removeFirst(){
+  E ret = data[start];
+  start++;
+  size--;
+return ret;
+ }
+public E removeLast(){
+  E ret = data[end];
+  end--;
+  size--;
+  return ret;
+ }
+public E getFirst(){
+return data[start];
+}
+public E getLast(){
+return data[start];
+}
 }
