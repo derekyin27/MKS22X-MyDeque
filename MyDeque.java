c52bb92651a256541f1570ed8af4973f6f8c5c9d
Derek <dyin00@stuy.edu>
2019-03-22 11:47:20 -0400

    comments

--- MyDeque.java
+++ MyDeque.java
@@ -5,7 +5,7 @@ private int size, start, end;
 @SuppressWarnings("unchecked")
 public MyDeque(){
   data = (E[])new Object[10];
-  size=10;
+  size=10;//default size of 10 for no parameter constructor
   start = 0;
   end = data.length;
   }
@@ -37,7 +37,7 @@ public String toString(){
  }
  private boolean isEmpty(){
    for (int i = 0; i < data.length; i++){
-     if (data[i] != null) return false;
+     if (data[i] != null) return false;//linear check for empty deque
    }
    return true;
  }
@@ -52,13 +52,13 @@ public void addFirst(E element){
 }
 
 public void addLast(E element){
-  if (element == null) throw new NullPointerException();
-  else if (end < data.length){
+  if (element == null) throw new NullPointerException();//checks if element is null
+  else if (end < data.length){//simply add the value to the right of last value
     data[end+1] = element;
     end++;
     size++;
   }
-  else if (data[data.length-1] != null){
+  else if (data[data.length-1] != null){//if the deque full, resize then add
     resize();
     data[end+1] = element;
     end++;
@@ -67,14 +67,14 @@ public void addLast(E element){
  }
 public E removeFirst(){
   if (isEmpty()) throw new NoSuchElementException();
-  else {E ret = data[start];
+  else {E ret = data[start];//removes first element and return that value
   start++;
   size--;
 return ret;
 }
 }
 public E removeLast(){
-  if (isEmpty()) throw new NoSuchElementException();
+  if (isEmpty()) throw new NoSuchElementException();//remove last element then return that value
   else {E ret = data[end];
   end--;
   size--;
@@ -83,10 +83,10 @@ public E removeLast(){
 }
 public E getFirst(){
   if (isEmpty()) throw new NoSuchElementException();
-else return data[start];
+else return data[start];//only returns first element
 }
 public E getLast(){
   if (isEmpty()) throw new NoSuchElementException();
-else return data[end];
+else return data[end];//returns last element only
 }
 }
