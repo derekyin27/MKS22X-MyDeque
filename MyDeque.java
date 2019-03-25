6d5a772ecbd6f3382190709ad183f6b78cf9ee36
Derek <dyin00@stuy.edu>
2019-03-24 21:49:14 -0400

    new resize

--- MyDeque.java
+++ MyDeque.java
@@ -42,11 +42,16 @@ else {
  }
  @SuppressWarnings("unchecked")
  private void resize(){
-   E[] resized = (E[])new Object[size*2];
-   for (int i = 0; i < data.length; i++){
-     resized[i] = data[i];
+   int counter = 0;
+   E[] resized = (E[])new Object[size*2+1];
+   for (int i = start; i < data.length; i++){
+     resized[counter++] = data[i];
+   }
+   for (int i = 0; i < start; i++){
+     resized[counter++] = data[i];
    }
    data = resized;
+   end = count-1;
  }
  private boolean isEmpty(){
    for (int i = 0; i < data.length; i++){
@@ -56,16 +61,25 @@ else {
  }
 public void addFirst(E element){
   if (element == null) throw new NullPointerException();
-  if (data[data.length] != null){
+  if (start==0 && size>= data.length-1){
     resize();
  }
- if (start < end){
-
+ else{
+   start = data.length-1;
+   data[data.length-1] = element;
+   size++;
  }
+ if (start !=0 && size!=data.length-1){
+   start--;
+   data[start] = element;
+   size++;
+ }
+
 }
 
 public void addLast(E element){
   if (element == null) throw new NullPointerException();//checks if element is null
+  if (size >= data.length) resize();
   else if (end < data.length){//simply add the value to the right of last value
     data[end+1] = element;
     end++;
