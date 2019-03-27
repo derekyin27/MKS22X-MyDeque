3a315cfedd8972a5c06b4fed6a0f1ddd1a6e2258
Derek <dyin00@stuy.edu>
2019-03-27 13:42:33 -0400

    worked on it

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,38 @@
 public class Calculator{
   public static double eval(String s){
-    MyDeque<String> stack = new MyDeque<>();
+    MyDeque<String> stack = new MyDeque<String>();
     String[] temp = s.split(" ");
     for (int i = 0; i < temp.length; i++){
+      double comp = 0.0;
       stack.addLast(temp[i]);
+      String next = stack.removeLast();
+      if (next.equals("+")){
+        double value = Double.parseDouble(stack.removeLast());
+        double value2 = Double.parseDouble(stack.removeLast());
+        comp = value + value2;
+        stack.addLast(comp + "");
+      }
+      else if (next.equals("-")){
+        double value = Double.parseDouble(stack.removeLast());
+        double value2 = Double.parseDouble(stack.removeLast());
+        comp = value - value2;
+        stack.addLast(comp + "");
+      }
+      else if (next.equals("*")){
+        double value = Double.parseDouble(stack.removeLast());
+        double value2 = Double.parseDouble(stack.removeLast());
+        comp = value * value2;
+        stack.addLast(comp + "");
+      }
+      else if (next.equals("/")){
+        double value = Double.parseDouble(stack.removeLast());
+        double value2 = Double.parseDouble(stack.removeLast());
+        comp = value / value2;
+        stack.addLast(comp + "");
+      }
+
     }
+  return Double.parseDouble(stack.getLast());
 
   }
 }
