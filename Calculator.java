3e3311d2925e6d6aa1569562fdf6a2720b67deb4
Derek <dyin00@stuy.edu>
2019-03-29 10:11:53 -0400

    comments

--- Calculator.java
+++ Calculator.java
@@ -12,15 +12,15 @@ public class Calculator{
       double comp = 0.0;
       String next = stack.removeLast();
       if (next.equals("+")){
-        double value = Double.parseDouble(stack.removeLast());
+        double value = Double.parseDouble(stack.removeLast());//take next 2 values
         double value2 = Double.parseDouble(stack.removeLast());
-        comp = value2 + value;
+        comp = value2 + value;//add them
         stack.addLast(comp + "");
       }
       else if (next.equals("-")){
         double value = Double.parseDouble(stack.removeLast());
         double value2 = Double.parseDouble(stack.removeLast());
-        comp = value2 - value;
+        comp = value2 - value;//subtract the values in THIS order, because it is postfix notation
         stack.addLast(comp + "");
       }
       else if (next.equals("*")){
@@ -32,13 +32,13 @@ public class Calculator{
       else if (next.equals("/")){
         double value = Double.parseDouble(stack.removeLast());
         double value2 = Double.parseDouble(stack.removeLast());
-        comp = value2 / value;
+        comp = value2 / value;//divide the values in THIS order because it is in postfix notation
         stack.addLast(comp + "");
       }
       else if (next.equals("%")){
         double value = Double.parseDouble(stack.removeLast());
         double value2 = Double.parseDouble(stack.removeLast());
-        comp = value2 % value;
+        comp = value2 % value;//mod values in this order because it is postfix
         stack.addLast(comp + "");
       }
     }
